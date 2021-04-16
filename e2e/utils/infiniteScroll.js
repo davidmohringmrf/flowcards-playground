0e31f5c4fd795e6c2e4d2d12468e638cdfc91434
David Valentín <davidvalentin@MacBook-Pro-50.local>
2021-04-16 12:25:08 +0200

    test(e2e): fix infinite scroll utility

--- e2e/utils/infiniteScroll.js
+++ e2e/utils/infiniteScroll.js
@@ -3,7 +3,7 @@
 const CARD_POSITIONER_TEST_ID = 'card-positioner';
 
 const triggerInfiniteScroll = async(browser)=>{
-	return await browser.executeAsync(async(args, done) => {
+	await browser.executeAsync(async(args, done) => {
 		window.scrollTo(0, document.body.scrollHeight);
 
 		done();
@@ -12,23 +12,23 @@ const triggerInfiniteScroll = async(browser)=>{
 
 const isAttachedToEndOfPage = async(browser, cardSelector) => {
 
-	const cardPositionetIsSticky = await browser.executeAsync(async(id, done) => {
+	const cardPositionetIsSticky = await browser.executeAsync(async(id, cardPositionerTestId, done) => {
 		const getCardPositioner = (el) => {
 			const parentEl = el.parentElement;
 
-			if (parentEl.getAttribute('data-testid') !== CARD_POSITIONER_TEST_ID) {
+			if (parentEl.getAttribute('data-testid') !== cardPositionerTestId) {
 				return getCardPositioner(parentEl);
 			}
 
 			return parentEl;
 		};
-
-		const cardPositioner = getCardPositioner(document.querySelector(id));
+		const card = document.querySelector(id);
+		const cardPositioner = getCardPositioner(card);
 
 		const isSticky = cardPositioner.getAttribute('style').includes('position: sticky');
 
 		done(isSticky);
-	}, cardSelector);
+	}, cardSelector, CARD_POSITIONER_TEST_ID);
 
 	return cardPositionetIsSticky;
 };
